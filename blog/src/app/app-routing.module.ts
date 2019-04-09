0f5af1de15a725044357c7fdf9e195c00905f1a2
Mateusz Wilk <mateuszwilk997@gmail.com>
2019-04-09 19:13:24 +0200

    complicated things

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -4,18 +4,25 @@ import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
+import { BlogHomeComponent } from './components/blog-home/blog-home.component';
 
 const routes: Routes = [{
   path: '',
   component: HomeComponent
 },
 {
+  path: 'blog/detail/:id',
+  component: BlogItemDetailsComponent
+  },
+
+{
   path: 'contact',
   component: ContactComponent,
 },
 {
   path: 'blog',
-  component: BlogComponent,
+  component: BlogHomeComponent,
 
 },
 {
